b6bf8b380cf484dc335b4749290a2365947aabe1
thiagoMboero <thiagomb4@al.insper.edu.br>
2024-04-26 15:02:07 -0300

    cria matriz (errado)

---
+++ Funções EP2/Cria matriz quadrada de espaços.py
@@ -0,0 +1,19 @@
+def cria_mapa(n):
+    listona = []
+    lista = []
+
+    for linha in listona:
+        if linha <= n:
+           lista = [''] * n
+           listona.append(lista)
+        else:
+           break
+       
+    return listona
+
+
+
+
+    
+
+    

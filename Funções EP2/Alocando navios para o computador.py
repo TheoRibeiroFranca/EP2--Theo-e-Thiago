c9ae030a8413cf5beb4fb4684f46af77a05ee356
thiagoMboero <thiagomb4@al.insper.edu.br>
2024-04-29 13:51:24 -0300

    Aloca navios começo (continuar)

---
+++ Funções EP2/Alocando navios para o computador.py
@@ -0,0 +1,58 @@
+import posicao_suporta 
+linha = random.randint(0, n-1)
+coluna = random.randint(0, n-1)
+orientacao = random.choice(['h', 'v'])
+
+def aloca_navios(mapa,blocos):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

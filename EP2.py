f3728c3e436bc4e7a407f3d1bf409f385fabaa6b
TheoRibeiroFranca <theorf@al.insper.edu.br>
2024-05-02 23:44:02 -0300

    tentando ajustar o mapa do computador

--- EP2.py
+++ EP2.py
@@ -2,11 +2,10 @@ import random
 from math import *
 
 # Navio pode ser alocado na posição
-import random
-def posicao_suporta(mapa, blocos, linha, coluna, orientação):
+def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
     if mapa[linha][coluna] == "N":
         return False
-    if orientação == "v":
+    if orientacao == "v":
         if (linha + blocos) > len(mapa):
             return False
         else:
@@ -14,18 +13,17 @@ def posicao_suporta(mapa, blocos, linha, coluna, orientação):
                 if mapa[i][coluna] == "N":
                     return False
             return True
-    if orientação == "h":
-        for linhas_mapa in mapa:  
-            if (coluna + blocos) > len(linhas_mapa):
-                return False
-            else:
-                for j in range(coluna, coluna + blocos):
-                    if mapa[linha][j] == "N":
-                        return False
-                return True
+    if orientacao == "h":
+        if (coluna + blocos) > len(mapa[linha]):
+            return False
+        else:
+            for j in range(coluna, coluna + blocos):
+                if mapa[linha][j] == "N":
+                    return False
+            return True
 
 # Alocando navios para o computador
-def aloca_navios(mapa,blocos):
+def aloca_navios(mapa, blocos):
     linhas = len(mapa)
     colunas = len(mapa[0])
     linha = random.randint(0, linhas - 1)
@@ -40,21 +38,23 @@ def aloca_navios(mapa,blocos):
             a = posicao_suporta(mapa, barcos, linha, coluna, orientacao)
         if orientacao == 'v':
             for i in range(linha, linha + barcos):
-                mapa[i][coluna] = ' N'
+                mapa[i][coluna] = 'N'
         if orientacao == 'h':
-            for j in range(coluna, coluna+barcos):
-                mapa[linha][j] = ' N'
+            for j in range(coluna, coluna + barcos):
+                mapa[linha][j] = 'N'
     return mapa
+
 
 # Cria matriz quadrada de espaços
 def cria_mapa(n):
     listona = []
-    linha_referencia = [' '] * n
+    linha_referencia = ['  '] * n
 
     for i in range(0,n):
         listona.append(linha_referencia)
     
     return listona
+
 
 # Verifica se acabou os 'N's da matriz
 def foi_derrotado(matriz):
@@ -64,7 +64,6 @@ def foi_derrotado(matriz):
                 return False
     return True
 
-
 #função para criar um mapa para o print
 def mapa_print(pc, jog, pais_pc, pais_jogador):
     return(f'''
@@ -73,9 +72,9 @@ def mapa_print(pc, jog, pais_pc, pais_jogador):
  1 {pc[0][0]} {pc[0][1]} {pc[0][2]} {pc[0][3]} {pc[0][4]} {pc[0][5]} {pc[0][6]} {pc[0][7]} {pc[0][8]} {pc[0][9]}  1    1 {jog[0][0]} {jog[0][1]} {jog[0][2]} {jog[0][3]} {jog[0][4]} {jog[0][5]} {jog[0][6]} {jog[0][7]} {jog[0][8]} {jog[0][9]}  1
  2 {pc[1][0]} {pc[1][1]} {pc[1][2]} {pc[1][3]} {pc[1][4]} {pc[1][5]} {pc[1][6]} {pc[1][7]} {pc[1][8]} {pc[1][9]}  2    2 {jog[1][0]} {jog[1][1]} {jog[1][2]} {jog[1][3]} {jog[1][4]} {jog[1][5]} {jog[1][6]} {jog[1][7]} {jog[1][8]} {jog[1][9]}  2
  3 {pc[2][0]} {pc[2][1]} {pc[2][2]} {pc[2][3]} {pc[2][4]} {pc[2][5]} {pc[2][6]} {pc[2][7]} {pc[2][8]} {pc[2][9]}  3    3 {jog[2][0]} {jog[2][1]} {jog[2][2]} {jog[2][3]} {jog[2][4]} {jog[2][5]} {jog[2][6]} {jog[2][7]} {jog[2][8]} {jog[2][9]}  3
- 4 {pc[3][0]} {pc[3][1]} {pc[3][3]} {pc[3][3]} {pc[3][4]} {pc[3][5]} {pc[3][6]} {pc[3][7]} {pc[3][8]} {pc[3][9]}  4    4 {jog[3][0]} {jog[3][1]} {jog[3][3]} {jog[3][3]} {jog[3][4]} {jog[3][5]} {jog[3][6]} {jog[3][7]} {jog[3][8]} {jog[3][9]}  4
- 5 {pc[4][0]} {pc[4][1]} {pc[4][4]} {pc[4][3]} {pc[4][4]} {pc[4][5]} {pc[4][6]} {pc[4][7]} {pc[4][8]} {pc[4][9]}  5    5 {jog[4][0]} {jog[4][1]} {jog[4][4]} {jog[4][3]} {jog[4][4]} {jog[4][5]} {jog[4][6]} {jog[4][7]} {jog[4][8]} {jog[4][9]}  5 
- 6 {pc[5][0]} {pc[5][1]} {pc[5][5]} {pc[5][3]} {pc[5][4]} {pc[5][5]} {pc[5][6]} {pc[5][7]} {pc[5][8]} {pc[5][9]}  6    6 {jog[5][0]} {jog[5][1]} {jog[5][5]} {jog[5][3]} {jog[5][4]} {jog[5][5]} {jog[5][6]} {jog[5][7]} {jog[5][8]} {jog[5][9]}  6
+ 4 {pc[3][0]} {pc[3][1]} {pc[3][2]} {pc[3][3]} {pc[3][4]} {pc[3][5]} {pc[3][6]} {pc[3][7]} {pc[3][8]} {pc[3][9]}  4    4 {jog[3][0]} {jog[3][1]} {jog[3][2]} {jog[3][3]} {jog[3][4]} {jog[3][5]} {jog[3][6]} {jog[3][7]} {jog[3][8]} {jog[3][9]}  4
+ 5 {pc[4][0]} {pc[4][1]} {pc[4][2]} {pc[4][3]} {pc[4][4]} {pc[4][5]} {pc[4][6]} {pc[4][7]} {pc[4][8]} {pc[4][9]}  5    5 {jog[4][0]} {jog[4][1]} {jog[4][2]} {jog[4][3]} {jog[4][4]} {jog[4][5]} {jog[4][6]} {jog[4][7]} {jog[4][8]} {jog[4][9]}  5 
+ 6 {pc[5][0]} {pc[5][1]} {pc[5][2]} {pc[5][3]} {pc[5][4]} {pc[5][5]} {pc[5][6]} {pc[5][7]} {pc[5][8]} {pc[5][9]}  6    6 {jog[5][0]} {jog[5][1]} {jog[5][2]} {jog[5][3]} {jog[5][4]} {jog[5][5]} {jog[5][6]} {jog[5][7]} {jog[5][8]} {jog[5][9]}  6
  7 {pc[6][0]} {pc[6][1]} {pc[6][2]} {pc[6][3]} {pc[6][4]} {pc[6][5]} {pc[6][6]} {pc[6][7]} {pc[6][8]} {pc[6][9]}  7    7 {jog[6][0]} {jog[6][1]} {jog[6][2]} {jog[6][3]} {jog[6][4]} {jog[6][5]} {jog[6][6]} {jog[6][7]} {jog[6][8]} {jog[6][9]}  7
  8 {pc[7][0]} {pc[7][1]} {pc[7][2]} {pc[7][3]} {pc[7][4]} {pc[7][5]} {pc[7][6]} {pc[7][7]} {pc[7][8]} {pc[7][9]}  8    8 {jog[7][0]} {jog[7][1]} {jog[7][2]} {jog[7][3]} {jog[7][4]} {jog[7][5]} {jog[7][6]} {jog[7][7]} {jog[7][8]} {jog[7][9]}  8
  9 {pc[8][0]} {pc[8][1]} {pc[8][2]} {pc[8][3]} {pc[8][4]} {pc[8][5]} {pc[8][6]} {pc[8][7]} {pc[8][8]} {pc[8][9]}  9    9 {jog[8][0]} {jog[8][1]} {jog[8][2]} {jog[8][3]} {jog[8][4]} {jog[8][5]} {jog[8][6]} {jog[8][7]} {jog[8][8]} {jog[8][9]}  9
@@ -94,10 +93,10 @@ def navio_jogador(mapa, barco, linha, coluna, orientação):
         a = posicao_suporta(mapa, barco, linha, coluna, orientação)
     if orientação == 'v':
         for i in range(linha, linha + barco):
-            mapa[i][coluna] = 'N'
+            mapa[i][coluna] = ' N'
     if orientação == 'h':
         for j in range(coluna, coluna+barco):
-            mapa[linha][j] = 'N'
+            mapa[linha][j] = ' N'
     return mapa
 # quantidade de blocos por modelo de navio
 CONFIGURACAO = {
@@ -288,10 +287,8 @@ if 'porta-avioes' in barcos_computador:
 if 'submarino' in barcos_computador:
     for n in range(barcos_computador["submarino"]):
         lista_barcos_computador.append(2)
-print(lista_barcos_computador)
 mapa_computador = aloca_navios(mapa_computador,lista_barcos_computador)
 print(mapa_computador)
-
 #alocando os barcos para o jogador
 barcos_jogador = PAISES[pais_jogador]
 lista_barcos_jogador = []
@@ -335,11 +332,13 @@ for i in range(len(lista_nomes_barcos_jogador)):
         print("Linha inválida")
         linha = input("qual linha")
     linha = lista_linhas_checagem.index(linha)
+    linha = int(linha)
     while coluna not in lista_colunas_checagem:
         print('Letra inválida')
         coluna = input("qual Letra")
         coluna = coluna.upper()
     coluna = lista_colunas_checagem.index(coluna)
+    coluna = int(coluna)
     orientação = input("qual orientação[h/v]")
     orientação = orientação.lower()
     while orientação not in lista_orientacao_checagem:

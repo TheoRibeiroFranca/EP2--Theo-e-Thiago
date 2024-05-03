c4b5718f89fa2e10ab5a7ef066682b48ac9471c2
thiagoMboero <thiagomb4@al.insper.edu.br>
2024-05-02 21:45:18 -0300

    Corrigindo posição suporta

--- EP2.py
+++ EP2.py
@@ -2,15 +2,16 @@ import random
 from math import *
 
 # Navio pode ser alocado na posição
+import random
 def posicao_suporta(mapa, blocos, linha, coluna, orientação):
-    if mapa[linha][coluna] == " N":
+    if mapa[linha][coluna] == "N":
         return False
     if orientação == "v":
         if (linha + blocos) > len(mapa):
             return False
         else:
             for i in range(linha, linha + blocos):
-                if mapa[i][coluna] == " N":
+                if mapa[i][coluna] == "N":
                     return False
             return True
     if orientação == "h":
@@ -19,7 +20,7 @@ def posicao_suporta(mapa, blocos, linha, coluna, orientação):
                 return False
             else:
                 for j in range(coluna, coluna + blocos):
-                    if mapa[linha][j] == " N":
+                    if mapa[linha][j] == "N":
                         return False
                 return True
 
